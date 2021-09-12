43d56dac08cf3f31e3fb4cb2f80ee5351a723a1f
Machuca Juan <machucajuangabriel@gmail.com>
2021-09-12 10:14:29 -0300

    function receivers

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Curso Golang
 
+### Structs - Methods (Functions Receivers)
+Enlaces de guía: https://tour.golang.org/methods/4- última actualización 12/09/2021
 ### GoRutinas
 Enlaces de guía: https://tour.golang.org/concurrency/2 - última actualización 12/09/2021
--- Structs/main.go
+++ Structs/main.go
@@ -8,11 +8,18 @@ type Employee struct {
 	name string
 }
 
+/* Functions Receiver and Pointer receiver*/
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
 func main() {
 	e := Employee{}
 
 	fmt.Printf("%v\n", e) // Go asigna valores al crear, ej: id 0, "" -> retornara {0 }
 	e.id = 1
 	e.name = "Juan Gabriel"
+
+	e.SetId(1995)
 	fmt.Printf("%v\n", e)
 }
