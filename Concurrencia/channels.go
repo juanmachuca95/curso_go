871c2d52f393df8baa420fb81de4293d896ed9e0
Machuca Juan <machucajuangabriel@gmail.com>
2021-09-16 22:53:08 -0300

    Tipos de canales

---
+++ Concurrencia/channels.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/**
+* Tipos de canales
+* Canales con buffer y si buffer
+ */
+
+func main() {
+	/* Primer bloque
+	c := make(chan int)
+
+	c <- 1
+
+	fmt.Println(<-c)
+
+	al ejecutar go run channels.go esto devolverá un error
+	fatal error: all goroutines are asleep - deadlock!
+
+	goroutine 1 [chan send]:
+	main.main()
+			/home/juan/Escritorio/go/curso_go/Concurrencia/channels.go:13 +0x59
+	exit status 2
+
+	ya que necesitamos recibir lo que envia el canal
+
+	*/
+
+	c := make(chan int, 3) // channel with buffer
+
+	c <- 1
+	c <- 5
+	c <- 5
+	// c <- 2 Mi buffer es de 3 si envio más de tres retornará error
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+
+}
